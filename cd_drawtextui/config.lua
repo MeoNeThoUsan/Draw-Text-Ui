772d54099c340f5d568000e2bcf7db7a1a1c1bec
ME#1000 <82929620+MeoNeThoUsan@users.noreply.github.com>
2022-12-24 00:18:55 +0300

    Add files via upload

---
+++ cd_drawtextui/config.lua
@@ -0,0 +1,178 @@
+Config = {}
+Config.Locations = {
+	[1] = {
+		coords = vector3(451.33, -976.25, 25.31),
+		distance = 4,
+		key = 38,
+		eventname = 'event:control:bennys',
+		text = '<b>[E] Police Workshop</b>'
+	},
+	[2] = {
+		coords = vector3(-36.62, -1049.26, 27.88),
+		distance = 5,
+		key = 38,
+		eventname = 'event:control:bennyss',
+		text = '<b>Bennys</b>'
+	},
+	[3] = {
+		coords = vector3(-191.84, -1162.11, 23.67),
+		distance = 5,
+		key = 38,
+		eventname = 'event:control:bennyss',
+		text = '<b>Impound (F1)</b>'
+	},
+	[4] = {
+		coords = vector3(438.75, -981.84, 30.69),
+		distance = 5,
+		key = 38,
+		eventname = 'event:control:benny1232ss',
+		text = '<b>Police Station</b>'
+	},
+	[5] = {
+		coords = vector3(-916.77, -2036.67, 9.5),
+		distance = 5,
+		key = 38,
+		eventname = 'event:control:benny123ss',
+		text = '<b>Vehicle Shop</b>'
+	},
+	[6] = {
+		coords = vector3(931.81, 42.5, 81.09),
+		distance = 8,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>Casino</b>'
+	},
+	[7] = {
+		coords = vector3(59.41, 18.31, 69.23),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennys9s',
+		text = '<b>Garage (F1)</b>'
+	},
+	[8] = {
+		coords = vector3(67.52, 6380.06, 31.24),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennys8s',
+		text = '<b>Garage (F1)</b>'
+	},
+	[9] = {
+		coords = vector3(-338.93, 287.59, 85.52),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennys7s',
+		text = '<b>Garage (F1)</b>'
+	},
+	[10] = {
+		coords = vector3(-307.51, -982.14, 31.08),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennys6s',
+		text = '<b>Garage (F1)</b>'
+	},
+	[11] = {
+		coords = vector3(-331.95, -772.09, 33.97),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennyss5',
+		text = '<b>Garage (F1)</b>'
+	},
+	[12] = {
+		coords = vector3(-79.66, -2007.28, 18.0),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennyss4',
+		text = '<b>Garage (F1)</b>'
+	},
+	[13] = {
+		coords = vector3(221.56, -794.09, 30.81),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennyss3',
+		text = '<b>Garage (F1)</b>'
+	},
+	[14] = {
+		coords = vector3(-1191.08, -1486.45, 4.38),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennyss1',
+		text = '<b>Garage (F1)</b>'
+	},
+	[15] = {
+		coords = vector3(-1138.36, -748.69, 19.59),
+		distance = 12,
+		key = 38,
+		eventname = 'event:control:bennyss2',
+		text = '<b>Garage (F1)</b>'
+	},
+	[16] = {
+		coords = vector3(562.12, 2744.21, 42.88),
+		distance = 8,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>Pet Shop</b>'
+	},
+	[17] = {
+		coords = vector3(-678.17, 5835.56, 17.33),
+		distance = 8,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>Hunting Shop</b>'
+	},
+	[18] = {
+		coords = vector3(216.45, -1391.59, 30.58),
+		distance = 8,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>Driving School</b>'
+	},
+	[19] = {
+		coords = vector3(-551.48, -193.23, 38.47),
+		distance = 14,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>City Hall</b>'
+	},
+	[20] = {
+		coords = vector3(304.93, -587.0, 43.28),
+		distance = 10,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>PillBox Hospital</b>'
+	},
+	[21] = {
+		coords = vector3(-703.6, 270.02, 83.15),
+		distance = 5,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>Real Estate</b>'
+	},
+	[22] = {
+		coords = vector3(481.85, -987.56, 31.05),
+		distance = 2,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>/Evidence Room</b>'
+	},
+	[23] = {
+		coords = vector3(461.82, -996.46, 30.69),
+		distance = 3,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>Clothing Room</b>'
+	},
+	[24] = {
+		coords = vector3(461.66, -988.15, 30.69),
+		distance = 3,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>C.O.P Office</b>'
+	},
+	[25] = {
+		coords = vector3(-289.23, -1926.46, 30.15),
+		distance = 8,
+		key = 38,
+		eventname = 'event:control:benny133ss',
+		text = '<b>MAZE BANK ARENA</b>'
+	},
+}
